430179b2ddec0d97a060a91843988d8c5d1b674b
roxblnfk <roxblnfk@ya.ru>
2024-07-19 16:08:32 +0400

    feat: add `software` command

---
+++ src/Command/Base.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Internal\DLoad\Command;
+
+use Internal\DLoad\Bootstrap;
+use Internal\DLoad\Service\Container;
+use Internal\DLoad\Service\Logger;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\StyleInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * @internal
+ */
+abstract class Base extends Command
+{
+    protected Logger $logger;
+    protected Container $container;
+
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output,
+    ): int {
+        $this->logger = new Logger($output);
+        $this->container = $container = Bootstrap::init()->withConfig(
+            xml: \dirname(__DIR__, 2) . '/dload.xml',
+            inputOptions: $input->getOptions(),
+            inputArguments: $input->getArguments(),
+            environment: \getenv(),
+        )->finish();
+        $container->set($input, InputInterface::class);
+        $container->set($output, OutputInterface::class);
+        $container->set(new SymfonyStyle($input, $output), StyleInterface::class);
+        $container->set($this->logger);
+
+        return Command::SUCCESS;
+    }
+}
