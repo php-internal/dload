05399937a7d76ef3c48876cc1dce83306e894ad3
roxblnfk <roxblnfk@ya.ru>
2024-08-02 23:48:02 +0400

    feat: add `protoc` and `protoc-gen-php-grpc` in the registry

--- tests/Unit/Module/Common/ArchitectureTest.php
+++ tests/Unit/Module/Common/ArchitectureTest.php
@@ -18,6 +18,7 @@ class ArchitectureTest extends TestCase
         yield ['temporal_cli_0.13.2_windows.amd644.tar.gz', null];
         yield ['roadrunner-2024.1.5-windows.zip', null];
         yield ['roadrunner-2024.1.5-linux-amd64.deb', Architecture::X86_64];
+        yield ['protoc-27.3-win64.zip', Architecture::X86_64];
     }
 
     #[DataProvider('provideBuildNames')]
