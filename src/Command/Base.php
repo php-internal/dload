4af6e81da69dfbb38e0532d0c9589ef0aa35ad16
Tobias Nyholm <tobias.nyholm@gmail.com>
2025-12-14 18:08:27 +0100

    Stop using deprecated Command::getDefaultName()

--- src/Command/Base.php
+++ src/Command/Base.php
@@ -7,6 +7,7 @@ namespace Internal\DLoad\Command;
 use Internal\DLoad\Bootstrap;
 use Internal\DLoad\Service\Container;
 use Internal\DLoad\Service\Logger;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -52,6 +53,22 @@ abstract class Base extends Command
     {
         parent::configure();
         $this->addOption('config', null, InputOption::VALUE_OPTIONAL, 'Path to the configuration file');
+    }
+
+    public static function getCommandName(): ?string
+    {
+        if (!class_exists(AsCommand::class)) {
+            // Fall back on lower Symfony versions
+            return self::getDefaultName();
+        }
+
+        if ($attributes = (new \ReflectionClass(static::class))->getAttributes(AsCommand::class)) {
+            /** @var AsCommand $attribute */
+            $attribute = $attributes[0]->newInstance();
+            return $attribute->name;
+        }
+
+        return null;
     }
 
     /**
